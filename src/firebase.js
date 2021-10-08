6230d86d127dea3505c02c99aa51a003bada66a9
Huzaifa dabir <73576043+huzaifa729@users.noreply.github.com>
2021-10-08 09:57:26 -0700

    init

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD-b-w0vYp7HP38b1LmUw9-6lBZ7BREN9A",
+    authDomain: "clone-42385.firebaseapp.com",
+    projectId: "clone-42385",
+    storageBucket: "clone-42385.appspot.com",
+    messagingSenderId: "986575027302",
+    appId: "1:986575027302:web:be9f2c767bc4fda9f441a7"
+  };
