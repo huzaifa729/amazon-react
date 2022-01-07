59ca730a99641425d3488aeef8132eff877700e9
Huzaifa dabir <73576043+huzaifa729@users.noreply.github.com>
2022-01-07 07:09:02 -0800

    init

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,10 @@
+// import firebase  from "firebase";
+
+import firebase from "firebase/compat/app"
+import "firebase/compat/auth"
+import "firebase/compat/firestore"
+
+
 const firebaseConfig = {
     apiKey: "AIzaSyD-b-w0vYp7HP38b1LmUw9-6lBZ7BREN9A",
     authDomain: "clone-42385.firebaseapp.com",
@@ -6,3 +13,14 @@ const firebaseConfig = {
     messagingSenderId: "986575027302",
     appId: "1:986575027302:web:be9f2c767bc4fda9f441a7"
   };
+
+
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+
+  const db = firebaseApp.firestore();
+  const auth = firebase.auth();
+
+
+  export { db , auth };
