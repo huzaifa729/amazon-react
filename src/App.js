0b789fb92f8cf1d346a2c1e5aa11db357887d4a2
Huzaifa dabir <73576043+huzaifa729@users.noreply.github.com>
2022-01-16 08:38:40 -0800

    init

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import Checkout from './Checkout';
 import Login from './Login';
 import { auth } from './firebase';
 import { useStateValue } from './StateProvider'
+import Payment from './Payment';
 
 function App() {
   const [{}, dispatch] = useStateValue();
@@ -49,6 +50,11 @@ function App() {
         <Route path="/checkout">
            <Header />
             <Checkout/>
+            </Route>
+
+            <Route path="/payment">
+           <Header />
+            <Payment/>
             </Route>
 
     <Route path='/'>
